a1346371594a1f9c7cf8d67ee1f8fbc7da49b3c0
David Mrnustik <david.mrnustik@hotmail.com>
2015-05-03 00:34:04 +0200

    added devise views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :movie
+  validates_presence_of :author, :rating
   
   def self.descending #class method instead of normal method
     order(created_at: :desc)
