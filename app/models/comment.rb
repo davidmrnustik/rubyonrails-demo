2fea1162f435fbfba94db758942af48356b02f36
David Mrnustik <david.mrnustik@hotmail.com>
2015-05-03 13:52:45 +0200

    comments valitation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :movie
   validates_presence_of :author, :rating
+  validates_numericality_of :rating
+  validates_length_of :rating, maximum: 2
   
   def self.descending #class method instead of normal method
     order(created_at: :desc)
